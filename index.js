7630569e5cee0d9ddaaacb210d2a09bd1786f619
Keisuke Ishigami <keisuke.130@icloud.com>
2021-04-09 07:55:17 +0900

    目標物を完成

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const line = require("@line/bot-sdk");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const config = {
+    channelSecret: "1a27013d82df6c3d297fc99779b2fb6e",
+    channelAccessToken: "91X3DBdfGPeKI13+WQEAK5LiwCKazzggiMjUlQshdam7isE9hPFXBrEooa2aQxWvsuEywbjaIeYo/F4cTBVygbr6C7Gv0FLqVumLdu+fmGqEaF1Q2NrT29No4IWPsDdCIfycIYvdSvZJgKTH+fMlEQdB04t89/1O/w1cDnyilFU="
+};
+const bot = new line.Client(config);
+
+// 占い結果を返す関数
+function fortuneTelling() {
+    const num = Math.floor(Math.random() * 10) + 1;
+
+    if (num === 0) {
+        return "大吉ですo(^▽^)o"
+    } else if (num === 1) {
+        return "中吉です(￣∀￣)"
+    } else if (num === 2) {
+        return "小吉です"
+    } else if (num === 3) {
+        return "吉です"
+    } else if (num === 4) {
+        return "末吉です"
+    } else if (num === 5) {
+        return "凶です(T_T)"
+    } else {
+        return "大凶です( ；∀；)"
+    }
+}
+
+app.listen(PORT);
+console.log("サーバを動かしています")
+
+app.get("/", (req, res) => res.send("Hello LINE BOT!(GET)"));
+
+app.post("/webhook", line.middleware(config), (req, res) => {
+    // 成功ステータスコード
+    res.sendStatus(200);
+
+    try {
+        // すべてのイベント処理のプロミスを格納する配列。
+        let events_processed = [];
+        // イベントオブジェクトを順次処理。
+        req.body.events.forEach((event) => {
+            // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+            if (event.type == "message" && event.message.type == "text"){
+                // ユーザーからのテキストメッセージが「占って」だった場合のみ反応。
+                if (event.message.text == "占って"){
+                    // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                    events_processed.push(bot.replyMessage(event.replyToken, {
+                        type: "text",
+                        text: fortuneTelling()
+                    }));
+                }
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
---
+++ test.js
@@ -0,0 +1,5 @@
+function test() {
+    return "keisuke";
+}
+
+console.log(test())
